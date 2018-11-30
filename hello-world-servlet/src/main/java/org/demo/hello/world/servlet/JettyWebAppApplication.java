ae350f8225e0f1b67791bdde8483b49a1c7b1812
run-zheng <run.zheng548@gmail.com>
2018-12-01 00:28:31 +0800

    servlet jsp web.xml jstl el

--- hello-world-servlet/src/main/java/org/demo/hello/world/servlet/JettyWebAppApplication.java
+++ hello-world-servlet/src/main/java/org/demo/hello/world/servlet/JettyWebAppApplication.java
@@ -10,17 +10,18 @@ import lombok.extern.slf4j.Slf4j;
 public class JettyWebAppApplication {
 	public static void main(String[] args) {
 		Server server = new Server(8080); 
-		//用WebAppContext可以支持servlet
 		
+		//支持JSP必须增加以下配置
 		Configuration.ClassList classlist = Configuration.ClassList
                 .setServerDefault( server );
         classlist.addBefore(
                 "org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
                 "org.eclipse.jetty.annotations.AnnotationConfiguration" );
-        
+        //用WebAppContext可以支持servlet
 		WebAppContext webApp = new WebAppContext(); 
 		webApp.setContextPath( "/" );
 		webApp.setResourceBase("./src/main/webapp");
+		//支持jstl和其他tag必须设置以下配置
 		webApp.setAttribute(
                 "org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
                 ".*/[^/]*servlet-api-[^/]*\\.jar$|.*/javax.servlet.jsp.jstl-.*\\.jar$|.*/[^/]*taglibs.*\\.jar$" );
